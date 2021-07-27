8c0aa87823a9dc63802f53b4d03f14815bdf79dc
priyanka <priyankabmurdeshwar@gmail.com>
2021-07-27 18:01:03 +0530

    pushing_relational_Operator_program.java

---
+++ RelationalOperatorDemo.java
@@ -0,0 +1,13 @@
+class RelationalOperatorDemo{
+ public static void main(String args[]) {
+	int p = 1000;
+	int q = 3000;
+
+System.out.println( p == q);
+System.out.println( p != q);
+System.out.println( p > q);
+System.out.println( p < q);
+System.out.println( p >= q);
+System.out.println( p <= q);
+}
+}
