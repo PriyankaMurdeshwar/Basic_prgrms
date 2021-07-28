1d189bc53f8a6e93caf3a8f673486ea2a784c29b
priyanka <priyankabmurdeshwar@gmail.com>
2021-07-28 22:06:45 +0530

    pushing ifElse_condition_program.java

---
+++ IfElseConditionDemo.java
@@ -0,0 +1,18 @@
+class IfElseConditionDemo{
+ 	public static void main(String args[]) {
+
+	int year = 2000;
+	if( (year%4==0 && year%100 !=0) || (year%4 == 0)) {
+	System.out.println( year + " is a leap year");
+}
+	else{
+	System.out.println( "not a leap year");
+}
+}
+}
+
+ 
+
+
+	
+ 	
