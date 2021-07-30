d2343af813137cca7dea3f07121dc2402f77875e
priyanka <priyankabmurdeshwar@gmail.com>
2021-07-30 21:46:27 +0530

    pushing nestedif program.java

---
+++ IfElseIfLadderDemo .java
@@ -0,0 +1,14 @@
+class IfElseIfLadderDemo{
+ public static void main(String args[]){
+	String trafficSignal = "red";
+	if( trafficSignal == "red") {
+		System.out.println( " please stop & wait");
+	}
+	else if( trafficSignal == "yellow") {
+		System.out.println( " ready to go");
+	}
+	else {
+		System.out.println( " you can go");
+	}
+}
+}
