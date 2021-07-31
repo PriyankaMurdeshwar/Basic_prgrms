f356a60e7a0bd5bd7bb5417e390ce155e4a3bbf4
priyanka <priyankabmurdeshwar@gmail.com>
2021-07-31 23:54:01 +0530

    pushing prime_number_program.java

---
+++ IsPrimeNumber.java
@@ -0,0 +1,23 @@
+class IsPrimeNumber{
+
+public static void main(String args[]){
+
+
+ for( int i=1; i<=50; i++) {
+int temp = 0;
+
+for( int j=2; j <= i-1 ; j++) {
+ 
+if( i%j == 0) {
+	temp = temp + 1;
+}
+}
+if ( temp == 0){
+System.out.println( i);
+}
+
+}
+}
+}
+
+	
