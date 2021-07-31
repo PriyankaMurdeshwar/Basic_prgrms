acf73b5611c180249d4de43c9e58d8732ace5d6e
priyanka <priyankabmurdeshwar@gmail.com>
2021-07-31 23:47:10 +0530

    pushing nested_break_loop_program.java

---
+++ MatrixA.java
@@ -0,0 +1,12 @@
+class MatrixA{
+public static void main(String args[]){
+	for(int i=1; i<=3; i++){
+	for(int j=1; j<=8; j++){
+	if(j%2 == 0){
+		System.out.print(j);
+	}
+	}
+	System.out.println();
+}
+}
+}
