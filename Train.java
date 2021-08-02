7a759706ce4b61ac094e3a9936ffb0c0e3da6777
priyanka <priyankabmurdeshwar@gmail.com>
2021-08-02 22:34:26 +0530

    pushing trai_behavior_program.java

---
+++ Train.java
@@ -0,0 +1,23 @@
+class Train{
+
+void speed(){
+System.out.println(" speed of train is 80km/h");
+}
+void sound(){
+System.out.println(" it makes noise ");
+}
+String getDetails(){
+return "Train name: Chennai Express\n"+
+	"Time: 6:30 AM\n"+
+	"Source: Bangalore\n"+
+	"Destination: Chennai\n" ;
+}
+
+void printGetDetails(){
+System.out.println("Train name: Chennai Express\n"+
+	"Time: 6:30 AM\n"+
+	"Source: Bangalore\n"+
+	"Destination: Chennai\n" );
+}
+}
+
---
+++ TrainTest.java
@@ -0,0 +1,9 @@
+class TrainTest{
+public static void main(String args[]){
+ Train train = new Train();
+train.sound();
+train.speed();
+System.out.println(train.getDetails());
+train.printGetDetails();
+}
+}
