294cde37478349162fec2abe5f9bf531dce04c44
priyanka <priyankabmurdeshwar@gmail.com>
2021-08-07 23:09:28 +0530

    hierarchical_inheritance_program.java

---
+++ hierarchical_inheritance/Activa.java
@@ -0,0 +1,8 @@
+package com.xworkz.java.inheritance.hierarchical_inheritance;
+
+public class Activa extends Honda {
+	void startStop() {
+		System.out.println(" Honda Activa gets an automatic engine start-stop function");
+	}
+
+}
---
+++ hierarchical_inheritance/Honda.java
@@ -0,0 +1,8 @@
+package com.xworkz.java.inheritance.hierarchical_inheritance;
+
+public class Honda {
+	void quality() {
+		System.out.println(" Honda usually has the better quality");
+	}
+	
+}
